8b2054e81ce9ea9adbccd3430e295a171f75449f
amonge20 <amonge20@jaumebalmes.net>
2022-12-19 20:04:33 +0100

    començant

---
+++ exercicis.js
@@ -0,0 +1,35 @@
+//EXERCICI 1 PART 1
+function createScoreBoard(){
+
+    function ScoreBoard(name, score){
+        this.name = name;
+        this.score = score;
+        this.createScoreBoard = createScoreBoard;
+    }
+
+var person = new ScoreBoard('Aitor', '10000')
+console.log(person);
+}
+//EXERCICI 1 PART 2
+function addPlayer(){
+    var score = ScoreBoard();
+}
+    
+//EXERCICI 1 PART 3
+//EXERCICI 1 PART 4
+//EXERCICI 1 PART 5
+//EXERCICI 1 PART 6
+
+//EXERCICI 2 PART 1
+//EXERCICI 2 PART 2
+//EXERCICI 2 PART 3
+//EXERCICI 2 PART 4
+//EXERCICI 2 PART 5
+//EXERCICI 2 PART 6
+
+//EXERCICI 3 PART 1
+//EXERCICI 3 PART 2
+//EXERCICI 3 PART 3
+//EXERCICI 3 PART 4
+//EXERCICI 3 PART 5
+//EXERCICI 3 PART 6
